916fadf64d1ea4c2c57112ece92a4392b79a82da
Sileadim <sileadim@Necromonion.(none)>
2013-12-16 15:17:03 +0100

    features

--- main.java
+++ main.java
@@ -1,26 +1,35 @@
 import br.ufpr.bioinfo.genbank.Entry;
+import br.ufpr.bioinfo.genbank.Feature;
 import br.ufpr.bioinfo.genbank.io.GenBankReader;
 import br.ufpr.bioinfo.genbank.parser.GenBankParser;
 
+import java.io.IOException;
 import java.lang.String;
+import java.util.List;
 
 
 
 public class main {
 
 		
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		readGene();
 
 	}
 	
-	public static void readGene()  
+	public static void readGene() throws IOException  
 	{
 		
-		String buffer = GenBankReader.readFile("/Data/coli.gb");
+		String buffer = GenBankReader.readFile("Data/coli.gb");
 		GenBankParser gp = new GenBankParser();
 		Entry entry = gp.process(buffer);
-		System.out.println("File read");
+		System.out.println(entry);
+		List<Feature>features = entry.getFeatures();
+		for(int i =0; i < features.size(); i++){
+		System.out.println(features.get(i).getKey());	
+		}
+
+		
 	}
 
 }
