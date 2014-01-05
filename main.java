da9def525c319a427a098512b6749cbf1810ba6e
Sileadim <sileadim@Necromonion.(none)>
2014-01-05 14:13:40 +0100

    gene und locations auf positiven strang

--- main.java
+++ main.java
@@ -25,8 +25,19 @@ public class main {
 		Entry entry = gp.process(buffer);
 		System.out.println(entry);
 		List<Feature>features = entry.getFeatures();
-		for(int i =0; i < features.size(); i++){
-		System.out.println(features.get(i).getKey());	
+		for(int i = 1; i < features.size(); i++){
+			
+		Feature f = features.get(i);
+		String featurename = f.getKey();
+		if(!f.getLocation().isComplement()){ 
+			if(featurename.equals("gene")){
+		
+		System.out.println(featurename);
+
+		System.out.println(f.getLocation().getMinor());		
+		System.out.println(f.getLocation().getMajor());	
+			}
+			}
 		}
 
 		
