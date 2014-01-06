a65f372f084d4bdbe516e4261872ddcab96954de
Sileadim <sileadim@Necromonion.(none)>
2014-01-06 15:36:21 +0100

    einfaches Hmm fertig

--- main.java
+++ main.java
@@ -1,6 +1,8 @@
 import be.ac.ulg.montefiore.run.jahmm.Hmm;
 import be.ac.ulg.montefiore.run.jahmm.Observation;
+import be.ac.ulg.montefiore.run.jahmm.ObservationInteger;
 import be.ac.ulg.montefiore.run.jahmm.Opdf;
+import be.ac.ulg.montefiore.run.jahmm.OpdfInteger;
 import br.ufpr.bioinfo.genbank.Entry;
 import br.ufpr.bioinfo.genbank.Feature;
 import br.ufpr.bioinfo.genbank.Sequence;
@@ -62,15 +64,100 @@ public class main {
 		//transistion probabilitys of going from state i to state j
 		double[][] a = calcTransitionProbs(seq, genePositions);
 		//List of Observation distributions
-		List<Opdf> opdfs;
-		int[][] counts;
+		List<Opdf> opdfs = calcEmissionProbs(seq, genePositions);
+		Hmm<ObservationInteger> hmm = new Hmm(pi,a, opdfs);
+		System.out.println(hmm.toString());
 	
-	
-		
-		
+			
 
 		
 	}
+	
+	public static List<Opdf> calcEmissionProbs(Sequence seq, List<int[]> genePositions)
+	{
+		List<Opdf> opdfs = new ArrayList<Opdf>();
+		String seqString = seq.getSequence();
+		
+		
+		//initialize emission probability matrix 
+		double[][] emProbs= new double[2][4];
+		//look at all characters
+		for(int i = 0; i < seqString.length(); i++ )
+		{
+			if( isInAGene(i,genePositions) )
+			{
+				if(seqString.charAt(i) == 'a')
+						{
+							emProbs[1][0]++;
+						}
+				if(seqString.charAt(i) == 'c')
+				{
+					emProbs[1][1]++;
+				}
+		
+				if(seqString.charAt(i) == 'g')
+				{
+					emProbs[1][2]++;
+				}
+				else
+				{
+					emProbs[1][3]++;
+				}
+			}
+			else
+			{
+				if(seqString.charAt(i) == 'a')
+						{
+							emProbs[0][0]++;
+						}
+				if(seqString.charAt(i) == 'c')
+				{
+					emProbs[0][1]++;
+				}
+		
+				if(seqString.charAt(i) == 'g')
+				{
+					emProbs[0][2]++;
+				}
+				else
+				{
+					emProbs[0][3]++;
+				}
+			}
+			
+			
+					
+		}
+		
+		//normalize over all emitted symbols in one state
+		double[] count = {(emProbs[0][0] + emProbs[0][1] + emProbs[0][2] +emProbs[0][3]),(emProbs[1][0] + emProbs[1][1] + emProbs[1][2] +emProbs[1][3])  };
+		for(int i = 0; i < emProbs.length; i ++)	
+		{
+			for(int j = 0; j < emProbs[0].length; j++)
+			{
+
+				emProbs[i][j] = emProbs[i][j]/count[i];
+				System.out.println("in " + i + " emit " + j + " with:" );
+				System.out.println(emProbs[i][j]);
+			}
+		}
+		
+		double[] probs0 = {emProbs[0][0], emProbs[0][1], emProbs[0][2],emProbs[0][3]};
+		double[] probs1 = {emProbs[1][0], emProbs[1][1], emProbs[1][2],emProbs[1][3]};
+		Opdf state0 = new OpdfInteger(probs0);
+		Opdf state1 = new OpdfInteger(probs1);
+		opdfs.add(state0);
+		opdfs.add(state1);
+		return opdfs;
+
+				
+	}
+	
+	
+	
+	
+	
+	
 	
 	public static double[][] calcTransitionProbs(Sequence seq, List<int[]> genePositions)
 	{
